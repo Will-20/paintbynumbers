51d4166a5d47c816f5c27004f5d343e3928e4575
Will-20 <williamguest42@icloud.com>
2024-09-16 04:13:00 +0100

    updated env variable

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,8 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+        destination: `${process.env.NEXT_PUBLIC_BACKEND}/api/:path*`
+        // destination: 
       },
     ]
   },
